50218e1d33f128f35d337e2b3f99522e23e7b170
RaffeLegend <oldgirafferiver@gmail.com>
2025-05-13 16:45:34 +0100

    update

--- src/load_eval.py
+++ src/load_eval.py
@@ -3,7 +3,7 @@ import json
 import os
 
 # System message for the assistant
-system_message = "You are an expert in misinformation detection area."
+system_message = "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the answer. The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think><answer> answer here </answer>"
 
 # User prompt that combines the user query and the schema
 user_prompt = """Please analyze the given image <start_of_image> containing both Chinese and English subtitles and complete the following three tasks: Classification Task: classify the alignment between the image and the subtitles into one of the following six categories:"All Consistent", "Image Manipulated","Both Misaligned","Chinese Misaligned", "English Misaligned","All Inconsistent".Manipulation Detection: if the image has been manipulated, return one or more bounding boxes for the manipulated regions in the format: {"bbox":[x\_min, y\_min, x\_max, y\_max]}. If no manipulation is found, return an empty list.Decision Explanation: explain your thinking step by step in EXPLANATION before the classification and any detected regions. Return your output using the following format, wrapped in tags:<think>EXPLANATION</think><answer>{"classification": RESULT, "region": {"bbox":[X_MIN,Y_MIN,X_MAX,Y_MAX]}}</answer>"""
@@ -19,15 +19,6 @@ user_prompt = """Please analyze the given image <start_of_image> containing both
 #     If no manipulation is found, return an empty list. \
 #     Return your output using the following format, wrapped in tags: \
 #     <answer>{"classification": RESULT, "region": {"bbox":[X_MIN,Y_MIN,X_MAX,Y_MAX]}}</answer>"""
-
-# response_format = """<think>...your detailed reasoning and step-by-step explanation...</think><answer>{{"classification": "...", "region": {{"bbox":["..."]}}}}</answer>"""
-response_format = """<answer>{{"classification": "...", "region": {{"bbox":["..."]}}}}</answer>"""
-
-def format(sample, response=response_format):
-    if sample["bbox"] is not None:
-        return response_format.format(RESULT=json.dumps(sample["label"]),BBOX=", ".join(str(x) for x in sample["bbox"]))
-    else:
-        return response_format.format(RESULT=json.dumps(sample["label"]),BBOX=None)
 
 # Convert dataset to OAI messages
 def format_data(sample, root_path):
