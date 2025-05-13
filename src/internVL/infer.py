e04204281e70ec99916d732efbaf16891b1f59f0
RaffeLegend <oldgirafferiver@gmail.com>
2025-05-13 22:40:44 +0100

    update

--- src/internVL/infer.py
+++ src/internVL/infer.py
@@ -1,20 +1,17 @@
 import os
 import torch
-from swift.llm import get_model_tokenizer, get_template, get_tokenizer
 from swift.tuners import Swift
-from swift.llm import InferEngine, InferRequest, PtEngine, RequestConfig, get_template
+from swift.llm import InferEngine, InferRequest, PtEngine, RequestConfig, get_template, get_model_tokenizer
 from transformers import GenerationConfig
 
 # 设置CUDA设备
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 # 模型相关参数
-base_model_id = "Qwen/Qwen2.5-3B-Instruct"
+model_id = "Qwen/Qwen2.5-3B-Instruct"
 output_dir = "/root/autodl-tmp/biqwen/"  # 训练输出路径
 checkpoint_dir = output_dir  # 可以设置为包含最后checkpoint的路径
 
-model_id_or_path = 'Qwen/Qwen2.5-3B-Instruct'  # model_id or model_path
-system = 'You are a helpful assistant.'
 infer_backend = 'pt'
 
 # 生成参数
@@ -23,9 +20,13 @@ temperature = 0
 stream = True
 
 # 加载模型和 tokenizer
-engine = PtEngine(model_id_or_path, adapters=[checkpoint_dir])
-template = get_template(engine.model.model_meta.template, engine.tokenizer, default_system=system)
-# 这里对推理引擎的默认template进行修改，也可以在`engine.infer`时进行传入
+model, tokenizer = get_model_tokenizer(model_id)
+model = Swift.from_pretrained(model, checkpoint_dir)
+
+template_type = model.model_meta.template
+template = get_template(template_type, tokenizer)
+engine = PtEngine.from_model_template(model, template, max_batch_size=2)
+
 engine.default_template = template
 
 def infer(engine: InferEngine, infer_request: InferRequest):
@@ -62,4 +63,3 @@ if __name__ == "__main__":
             "images": [image_path]  # 顶层放图像路径，List[str]
         }
         response = infer(engine, InferRequest(messages=messages_data['messages'], images=messages_data['images']))
-        print("result\n", response)
