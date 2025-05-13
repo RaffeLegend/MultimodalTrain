bc81f520fffdac5cf0a4c0915ed8b727cf55526c
RaffeLegend <oldgirafferiver@gmail.com>
2025-05-13 22:24:44 +0100

    update

--- src/internVL/infer.py
+++ src/internVL/infer.py
@@ -2,6 +2,7 @@ import os
 import torch
 from swift.llm import get_model_tokenizer, get_template, get_tokenizer
 from swift.tuners import Swift
+from swift.llm import InferEngine, InferRequest, PtEngine, RequestConfig, get_template
 from transformers import GenerationConfig
 
 # 设置CUDA设备
@@ -12,40 +13,28 @@ base_model_id = "Qwen/Qwen2.5-3B-Instruct"
 output_dir = "/root/autodl-tmp/biqwen/"  # 训练输出路径
 checkpoint_dir = output_dir  # 可以设置为包含最后checkpoint的路径
 
+model_id_or_path = 'Qwen/Qwen2.5-3B-Instruct'  # model_id or model_path
+system = 'You are a helpful assistant.'
+infer_backend = 'pt'
+
+# 生成参数
+max_new_tokens = 512
+temperature = 0
+stream = True
+
 # 加载模型和 tokenizer
-model = Swift.from_pretrained(base_model_id, checkpoint_dir, torch_dtype="bfloat16", device_map="auto")
-tokenizer = get_tokenizer(base_model_id)
-template = get_template(model.model_meta.template, tokenizer)
+engine = PtEngine(model_id_or_path, adapters=[checkpoint_dir])
+template = get_template(engine.model.model_meta.template, engine.tokenizer, default_system=system)
+# 这里对推理引擎的默认template进行修改，也可以在`engine.infer`时进行传入
+engine.default_template = template
 
-# 加载 LoRA 微调权重
-model = Swift.from_pretrained(model, checkpoint_dir)
-
-# 设置模板为推理模式
-template.set_mode('eval')
-
-# 推理函数
-def infer(prompt, max_new_tokens=512, temperature=0.7, top_p=0.9):
-    # 构造 prompt 和 tokenized 输入
-    input_data = {'input': prompt}
-    input_dict = template.encode(input_data, add_special_tokens=True)
-    input_ids = torch.tensor([input_dict['input_ids']], device=model.device)
-
-    # 设置生成配置
-    generation_config = GenerationConfig(
-        do_sample=True,
-        temperature=temperature,
-        top_p=top_p,
-        max_new_tokens=max_new_tokens,
-        pad_token_id=tokenizer.pad_token_id,
-        eos_token_id=tokenizer.eos_token_id,
-    )
-
-    # 生成输出
-    with torch.no_grad():
-        output_ids = model.generate(input_ids=input_ids, generation_config=generation_config)[0]
-    output_text = tokenizer.decode(output_ids, skip_special_tokens=True)
-
-    return output_text
+def infer(engine: InferEngine, infer_request: InferRequest):
+    request_config = RequestConfig(max_tokens=max_new_tokens, temperature=temperature)
+    resp_list = engine.infer([infer_request], request_config)
+    query = infer_request.messages[0]['content']
+    response = resp_list[0].choices[0].message.content
+    print(f'query: {query}')
+    print(f'response: {response}')
 
 # 示例推理
 from data_parser import user_prompt, system_message
@@ -55,8 +44,8 @@ if __name__ == "__main__":
     for sample in dataset:
         image_path = sample.get("captioned_path")
         # 构造 assistant 的 response 内容
-        bbox_values = ", ".join(str(x) for x in sample["bbox"]) if sample["bbox"] else ""
-        assistant_response = f"<think></think><answer>{{\"classification\": {json.dumps(sample['label'])}, \"region\": {{\"bbox\":[{bbox_values}]}}}}</answer>"
+        # bbox_values = ", ".join(str(x) for x in sample["bbox"]) if sample["bbox"] else ""
+        # assistant_response = f"<think></think><answer>{{\"classification\": {json.dumps(sample['label'])}, \"region\": {{\"bbox\":[{bbox_values}]}}}}</answer>"
         
         # 组装最终结构
         messages_data = {
@@ -72,5 +61,5 @@ if __name__ == "__main__":
             ],
             "images": [image_path]  # 顶层放图像路径，List[str]
         }
-        response = infer(messages_data)
+        response = infer(engine, InferRequest(messages=messages_data['messages'], images=messages_data['images']))
         print("result\n", response)
